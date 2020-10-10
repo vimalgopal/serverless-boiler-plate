0f90870969194366feedacdb9cb244d301340f33
Vimal <vimal251991@gmail.com>
2020-10-10 11:49:32 +0530

    First sample route

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Yay!! Step 1 Completed" );
+    res.send( "Yay!! Step 1: Typescript Initialisation, Completed" );
 } );
 
 // start the Express server
